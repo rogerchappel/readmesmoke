0d824f1f7b34b3d6dead19ba673b74e8ec35ae78
Roger Chappel <miscanalysis@gmail.com>
2026-05-12 18:42:42 +1000

    feat: expose library API

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+export { defaultConfig, loadConfig, normalizeConfig } from './config.js';
+export { expandGlobs, globToRegExp } from './globs.js';
+export { parseMarkdown, parseMarkdownFile, extractCommands } from './parser.js';
+export { inspectRisk, isDenied } from './risk.js';
+export { plan } from './planner.js';
+export { runPlan, runCommand } from './runner.js';
+export { buildReport, renderMarkdown, renderReport } from './report.js';
+export type * from './types.js';
